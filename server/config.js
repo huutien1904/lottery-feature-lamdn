2bbf2258975c36b6bda8d395aaa35c3306e3ec05
tienn <huutienn46@gmail.com>
2026-05-05 11:11:27 +0700

    update

--- server/config.js
+++ server/config.js
@@ -3,69 +3,68 @@ const prizes = [
     type: 0,
     count: 0,
     title: "Giải đặc biệt",
-    text: "Giải đặc biệt"
+    text: "Giải đặc biệt",
   },
   {
     type: 1,
     count: 1,
     title: "Giải đặc biệt",
     text: "Giải đặc biệt",
-    img: "../img/1.png"
+    img: "../img/1.png",
   },
   {
     type: 2,
     count: 1,
     text: "Giải nhất",
     title: "Giải nhất",
-    img: "../img/1.png"
+    img: "../img/1.png",
   },
   {
     type: 3,
-    count: 2,
+    count: 5,
     text: "Giải nhì",
     title: "Giải nhì",
-    img: "../img/2.png"
+    img: "../img/2.png",
   },
   {
     type: 4,
-    count: 3,
+    count: 10,
     text: "Giải ba",
     title: "Giải ba",
-    img: "../img/3.png"
+    img: "../img/3.png",
   },
-  {
-    type: 5,
-    count: 10,
-    text: "Giải tư",
-    title: "Giải tư",
-    img: "../img/4.png"
-  },
-  {
-    type: 6,
-    count: 40,
-    text: "Giải năm",
-    title: "Giải năm",
-    img: "../img/4.png"
-  },
-  {
-    type: 7,
-    count: 60,
-    text: "Giải khuyến khích",
-    title: "Giải khuyến khích",
-    img: "../img/5.png"
-  },
-
+  // {
+  //   type: 5,
+  //   count: 10,
+  //   text: "Giải tư",
+  //   title: "Giải tư",
+  //   img: "../img/4.png"
+  // },
+  // {
+  //   type: 6,
+  //   count: 40,
+  //   text: "Giải năm",
+  //   title: "Giải năm",
+  //   img: "../img/4.png"
+  // },
+  // {
+  //   type: 7,
+  //   count: 60,
+  //   text: "Giải khuyến khích",
+  //   title: "Giải khuyến khích",
+  //   img: "../img/5.png"
+  // },
 ];
 
 /**
  * 一Số giải mỗi lần quay
  */
-const EACH_COUNT = [1, 1, 1, 2,3, 10, 10, 10];
+const EACH_COUNT = [1, 1, 1, 2, 5, 10, 10, 10];
 
-const COMPANY = "MOBIFONE";
+const COMPANY = "VNSKY";
 
 module.exports = {
   prizes,
   EACH_COUNT,
-  COMPANY
+  COMPANY,
 };
